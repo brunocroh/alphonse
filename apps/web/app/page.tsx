db10e5915ca94dc72338cf44ee49734b1e6ea388
Bruno Rodrigues <brunocroh@gmail.com>
2024-02-24 21:45:59 -0300

    chore(web): remove some unused styles

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,17 +1,14 @@
 /* eslint-disable no-undef */
 import "alphonse/dist/index.css";
-import styles from "./page.module.css";
 import { Editor } from "./components/Editor";
 
 export default async function Page(): JSX.Element {
   return (
-    <main className={styles.main}>
-      <div className="container w-full">
-        <header>
-          <h1>Header</h1>
-        </header>
-        <Editor />
-      </div>
+    <main className="container p-2">
+      <header>
+        <h1>Header</h1>
+      </header>
+      <Editor />
     </main>
   );
 }
