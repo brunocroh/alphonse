cfc847d4fdfe5290a532ef4340fe400b2a041c54
Bruno Rodrigues <brunocroh@gmail.com>
2024-02-27 22:21:31 -0300

    feat(web): add padding and max width and heigth to page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,7 +5,7 @@ import { ThemeSwitcher } from "../components/theme-switcher";
 
 export default async function Page(): JSX.Element {
   return (
-    <main className="container p-2">
+    <main className="container screen-h-2xl min-h-screen max-w-screen-lg p-5">
       <header className="flex justify-between mb-5">
         <h1>Header</h1>
         <ThemeSwitcher />
